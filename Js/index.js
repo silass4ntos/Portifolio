bdaa9da034e162f860b51adee1be0bf9161ab615
silass4ntos <silas.moura.2013@hotmail.com>
2025-04-04 23:03:59 -0300

    up

--- Js/index.js
+++ Js/index.js
@@ -11,7 +11,7 @@ window.addEventListener('scroll', function () {
             // Força o reflow (repintura) para garantir que a transição seja aplicada
             void nav.offsetWidth;
             // Inicia a transição
-            nav.style.transition = 'all 0.3s ease';
+            nav.style.transition = 'all 0.5s ease';
         }
     } else {
         // Remove a classe quando a navegação volta ao topo
@@ -21,9 +21,42 @@ window.addEventListener('scroll', function () {
             // Força o reflow (repintura) para garantir que a transição seja aplicada
             void nav.offsetWidth;
             // Inicia a transição
-            nav.style.transition = 'all 0.3s ease';
+            nav.style.transition = 'all 0.5s ease';
         }
     }
 });
 
 
+//slow scroll
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); // Evita o comportamento padrão do link
+        const targetId = this.getAttribute('href').substring(1); // Pega o ID sem o "#"
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
+    });
+});
+
+// Destacar link ativo ao rolar a página
+const sections = document.querySelectorAll('section[id]');
+const scrollActive = () => {
+    const scrollY = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionLink = document.querySelector('.navbar a[href="#' + sectionId + '"]');
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            sectionLink.classList.add('active-link');
+        } else {
+            sectionLink.classList.remove('active-link');
+        }
+    });
+};
+
+window.addEventListener('scroll', scrollActive);
